de3bd4de9564013cbbf04ffd6f377fb8a7b11127
MehrabHamidi99 <mehrabhamidi78@gmail.com>
2023-11-24 21:25:12 -0500

    cleaning

--- run_job_width_wise.py
+++ run_job_width_wise.py
@@ -1,77 +1,57 @@
 from Experiments import *
+from utils import *
 from tqdm import tqdm
 import multiprocessing 
 import time
 import os
 from functools import partial
 
-constant_width = 50
-constant_depth = 4
-archs = [(constant_width + i * 10, [constant_width + i * 10 for _ in range(constant_depth)]) for i in range(10)]
+counter_number = 30
+step = 5
 
-constant_depth = 10
-archs2 = [(constant_width + i * 10, [constant_width + i * 10 for _ in range(constant_depth)]) for i in range(10)]
+constant_width = 5
+constant_depth = 3
+archs_1 = [(constant_width + i * step, [constant_width + i * step for _ in range(constant_depth)]) for i in range(counter_number)]
 
 
-# archs = [
-#     (100, [100]),
-#     (100, [100, 100]),
-#     (100, [100, 100, 100]),
-#     (100, [100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
-#     (100, [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]),
+constant_width = 5
+constant_depth = 12
+archs_2 = [(constant_width + i * step, [constant_width + i * step for _ in range(constant_depth)]) for i in range(counter_number)]
 
-# ]
+
+constant_width = 5
+constant_depth = 64
+archs_3 = [(constant_width + i * step, [constant_width + i * step for _ in range(constant_depth)]) for i in range(counter_number)]
+
+
+def run_function(arch_set, dest='results/tests'):
+    pool = multiprocessing.Pool(processes=40)
+    prod_x=partial(one_random_experiment, exps=50, num=10000, one=False, pre_path=dest, normal_dist=normal_dist, loc=0, scale=scale)
+    result_list = pool.map(prod_x, arch_set)
+
+    p_path = dest
+    if normal_dist:
+        p_path +=  'normal_std{}/'.format(str(scale))
+
+    animate_histogram(result_list, 'hidden Layers',  pre_path=p_path)
+    pool.close()
 
 if __name__ == '__main__':
     scale = 1
     normal_dist = True
 
     starttime = time.time()
-    pool = multiprocessing.Pool(processes=40)
-    prod_x=partial(one_random_experiment, exps=50, num=10000, one=False, pre_path='results/width_analysis/', normal_dist=normal_dist, loc=0, scale=scale)
-    result_list = pool.map(prod_x, archs)
 
-    p_path = 'results/width_analysis/'
-    if normal_dist:
-        p_path +=  'normal_std{}/'.format(str(scale))
+    print('with analysis shallow...')
+    run_function(archs_1, 'results/width_analysis/shallow/')
+    print("Done")
 
-    animate_histogram(result_list, 'hidden Layers',  pre_path=p_path)
+    print('with analysis deep...')
+    run_function(archs_2, 'results/width_analysis/deep/')
+    print("Done")
 
-    pool.close()
-    print("second batch")
+    print('with analysis deeper...')
+    run_function(archs_3, 'results/width_analysis/deeper/')
+    print("Done")
 
-    pool = multiprocessing.Pool(processes=40)
-    prod_x=partial(one_random_experiment, exps=50, num=10000, one=False, pre_path='results/width_analysis1/', normal_dist=normal_dist, loc=0, scale=scale)
-    result_list = pool.map(prod_x, archs2)
-
-    p_path = 'results/width_analysis1/'
-    if normal_dist:
-        p_path +=  'normal_std{}/'.format(str(scale))
-
-    animate_histogram(result_list, 'hidden Layers',  pre_path=p_path)
-
-    pool.close()
     print('That took {} seconds'.format(time.time() - starttime))
